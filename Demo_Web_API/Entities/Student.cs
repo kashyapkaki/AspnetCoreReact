23530320f8fc0babcd12b3b1133d1d757760ed89
Kashyap Kaki <kashyap.kaki2@boeing.com>
2021-08-10 11:28:44 +0530

    Technical Test - API code changes has been included

---
+++ Demo_Web_API/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Web_API.Entities
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RollNo { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Demo_Web_API/Services/StudentService.cs
@@ -0,0 +1,49 @@
+﻿using Demo_Web_API.Context;
+using Demo_Web_API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Web_API.Services
+{
+    public class StudentService : IStudentService
+    {
+        public StudentContext _studentDbContext;
+        public StudentService(StudentContext studentDbContext)
+        {
+            _studentDbContext = studentDbContext;
+        }
+        public Student AddStudent(Student student)
+        {
+            _studentDbContext.Student.Add(student);
+            _studentDbContext.SaveChanges();
+            return student;
+        }
+        public List<Student> GetStudents()
+        {
+            return _studentDbContext.Student.ToList();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            _studentDbContext.Student.Update(student);
+            _studentDbContext.SaveChanges();
+        }
+
+        public void DeleteStudent(int Id)
+        {
+            var student = _studentDbContext.Student.FirstOrDefault(x => x.Id == Id);
+            if (student != null)
+            {
+                _studentDbContext.Remove(student);
+                _studentDbContext.SaveChanges();
+            }
+        }
+
+        public Student GetStudent(int Id)
+        {
+            return _studentDbContext.Student.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
